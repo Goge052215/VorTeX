360ff67004b10422b64a2434ded5d2d23c2a7ddc
George Huang <154580011+Goge052215@users.noreply.github.com>
2025-01-17 23:33:16 +0800

    Update fonts_download.bash

--- fonts/fonts_download.bash
+++ fonts/fonts_download.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Check if Homebrew is installed
 if ! command -v brew &> /dev/null; then
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -8,9 +7,7 @@ else
     echo "Homebrew is already installed."
 fi
 
-# Install font using the new tap
 echo "Installing the Monaspace Neon font..."
-brew tap homebrew/cask-fonts || true  # Continue even if tap exists
-brew install --cask font-monaspace || true  # Install font, continue if already installed
-
+brew tap homebrew/cask-fonts || true
+brew install --cask font-monaspace || true
 echo "Font installation complete."
